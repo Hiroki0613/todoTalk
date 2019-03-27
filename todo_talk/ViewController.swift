8fbae8cd0a1fcf2cb717bd68d5d175ee34548bfb
Hiroki Kondo <kondo.matr02@gmail.com>
2019-03-27 23:50:06 +0900

    Initial Commit

---
+++ todo_talk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo_talk
+//
+//  Created by 宏輝 on 27/03/2019.
+//  Copyright © 2019 Hiroki Kondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
