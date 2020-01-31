b35d55f637013b46e675a295ae27ac9841bd74c7
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-31 22:31:12 +0900

    InputVoiceSystem.swiftの追加

---
+++ todo_talk/Controller/InputVoiceSystem.swift
@@ -0,0 +1,13 @@
+//
+//  InputVoiceSystem.swift
+//  todo_talk
+//
+//  Created by 近藤宏輝 on 2020/01/31.
+//  Copyright © 2020 Hiroki Kondo. All rights reserved.
+//
+
+import Foundation
+
+class InputVoiceSystem(){
+    
+}
